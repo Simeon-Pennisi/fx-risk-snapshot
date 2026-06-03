c4c155a25c44d8dcee2978f33744528d025c4d05
Simeon Pennisi <simeonpennisi@gmail.com>
2026-06-03 07:07:49 -0400

    Fix: Spring Initializr

---
+++ fx-risk-snapshot-api/src/main/java/com/simeon/fx_risk_snapshot_api/FxRiskSnapshotApiApplication.java
@@ -0,0 +1,13 @@
+package com.simeon.fx_risk_snapshot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxRiskSnapshotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxRiskSnapshotApiApplication.class, args);
+	}
+
+}
---
+++ fx-risk-snapshot-api/src/test/java/com/simeon/fx_risk_snapshot_api/FxRiskSnapshotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simeon.fx_risk_snapshot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxRiskSnapshotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
